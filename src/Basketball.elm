c400e00458472b17d4ff31a41ac04bfa241277c6
Karlo Smid <karlo.smid@gmail.com>
2020-06-08 15:41:59 +0200

    compare values in elm.

--- src/Basketball.elm
+++ src/Basketball.elm
@@ -1,3 +1,10 @@
-module Basketball exposing(trials)
+module Basketball exposing( position, trials)
 
-trials height = if height > 180 then "invite candidate" else "reject candidate"
+trials height =
+    if height > 180 then "invite candidate"
+    else "reject candidate"
+
+position height =
+    if height > 205 then "center"
+    else if height > 200 then "forward"
+    else "guard"
