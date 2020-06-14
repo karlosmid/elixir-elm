6a2fddf17ff9460f53d197bade9978949406174c
Karlo Smid <karlo.smid@gmail.com>
2020-06-14 14:21:46 +0200

    basketball letter function is using as input two functions.

--- src/Basketball.elm
+++ src/Basketball.elm
@@ -1,4 +1,4 @@
-module Basketball exposing( position, trials)
+module Basketball exposing( position, trials, letter)
 
 trials height =
     if height > 180 then "invite candidate"
@@ -7,4 +7,6 @@ trials height =
 position height =
     if height > 205 then "center"
     else if height > 200 then "forward"
-    else "guard"
+    else if height > 180 then "guard"
+    else String.fromInt height
+letter first second height = first height ++ " as " ++ second height
