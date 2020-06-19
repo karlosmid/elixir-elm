66d1c2a3ee7f00b9d8b263af671715105794de05
Karlo Smid <karlo.smid@gmail.com>
2020-06-19 14:39:35 +0200

    @moduledoc and @doc attributes.

--- in_action/module_attribute.ex
+++ in_action/module_attribute.ex
@@ -1,15 +1,18 @@
 defmodule Croatian.Basketball do
+  @moduledoc "Helps Croatian basketball teams in candidate selection."
+  @treshold 180
+  @doc "Based on candidate height, determines candidate team position."
   def position height do
     cond do
       height > 205 -> "center"
       height > 200 -> "forward"
-      height > 180 -> "guard"
+      height > @treshold -> "guard"
       true -> Integer.to_string(height)
     end
   end
-
+  @doc "Based on candidate height, invites or rejects candidate."
   def trials height do
-    if height > 180 do "invite candidate"
+    if height > @treshold do "invite candidate"
     else "reject candidate"
     end
   end
