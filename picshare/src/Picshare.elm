e45c2a082558ca7ff5d11ecc8dc0988ebf503a1b
Karlo Smid <karlo.smid@gmail.com>
2020-07-31 15:59:03 +0200

    add to Picshare.elm main variable h1 Picshare text as a header.

--- picshare/src/Picshare.elm
+++ picshare/src/Picshare.elm
@@ -1,6 +1,13 @@
 module Picshare exposing (main)
-import Html exposing (Html, div, text)
+-- 3. expose from Html Module h1 function
+import Html exposing (Html, div, text, h1)
+-- 1. import Html.Attributes module function class. class html attribute
+-- tells browser which css to use for element owner of class attribute
+import Html.Attributes exposing (class)
 
 main : Html msg
 main =
-    div [] [text "Picshare"]
+-- 2. use class header for our root div element
+    div [ class "header" ]
+-- 4. make text function child of h1 function, note that h1 attribute list is empty
+    [ h1 [] [text "Picshare"] ]
