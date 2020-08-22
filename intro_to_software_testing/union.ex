3c9255baa3309b490b38833dbd42b804a66beed2
Karlo Smid <karlo.smid@gmail.com>
2020-08-22 11:01:41 +0200

    add error handling to union

--- intro_to_software_testing/union.ex
+++ intro_to_software_testing/union.ex
@@ -6,7 +6,8 @@ defmodule IntroToSoftwareTesting do
   union returns list of elements that are present in either input list
   """
   @spec union(list(), list()) :: list()
-  def union(a, b) do
+  def union(a, b) when is_list(a) and is_list(b) do
     a ++ b |> Enum.uniq()
   end
+  def union(_a, _b), do: {:error, "input paramaters must be a list."}
 end
