dd089eaa74c2cc1c5166fd3d42f96574cfd967bb
Karlo Smid <karlo.smid@gmail.com>
2020-09-01 14:58:32 +0200

    add View to Picshare.

--- picshare/src/Picshare.elm
+++ picshare/src/Picshare.elm
@@ -2,30 +2,34 @@ module Picshare exposing (main)
 import Html exposing (..)
 import Html.Attributes exposing (class, src)
 
+-- 4. new main varialble that is using View
 main : Html msg
 main =
+    view initialModel
+-- 1. make main => view that has model as argument
+view : { url : String, caption : String } -> Html msg
+view model =
     div []
     [
         div [ class "header" ]
             [ h1 [] [text "Picshare"] ],
         div [ class "content-flow"]
-            [ viewPhoto (baseUrl ++ "27/kuce.folder/planinarska-kuca-picelj.jpg") "Picelj Park Near Zabok", 
-              viewPhoto (baseUrl ++ "69/_resized/dinara_475_0_withoutgrow.jpg") "Dinara Mountain"
-            ]
+            [ viewDetailedPhoto model]
     ]
-viewPhoto : String -> String -> Html msg
-viewPhoto url description =
+-- 2. accept model in viewDetailed photo function
+viewDetailedPhoto : { url : String, caption : String } -> Html msg
+viewDetailedPhoto  model =
               div [ class "detaile-photo" ]
                 [
-                img [ src url ] [],
+              -- 3. use model attribute url
+                img [ src model.url ] [],
                 div [ class "photo-info"]
-                    [ h2 [ class "caption" ] [ text description] ]
+                -- 4. use model attribute caption
+                    [ h2 [ class "caption" ] [ text model.caption] ]
                 ]
 baseUrl : String
 baseUrl = "https://www.hps.hr/files/data/"
--- 1. add initial model that describes a picture with record type annotation 
 initialModel : { url : String, caption : String }
--- 2. add record instance that represents picture for Park Picelj
 initialModel = 
     { url = baseUrl ++ "27/kuce.folder/planinarska-kuca-picelj.jpg"
       , caption = "Picelj Park Near Zabok"
