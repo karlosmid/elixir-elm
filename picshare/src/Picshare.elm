8e82ce66e7439d826b25aaed48f1dd650ae917fe
Karlo Smid <karlo.smid@gmail.com>
2020-12-23 13:11:53 +0100

    create HTTP request.

--- picshare/src/Picshare.elm
+++ picshare/src/Picshare.elm
@@ -1,12 +1,12 @@
--- 8. temporary expose to test in elm repl
-module Picshare exposing (main, photoDecoder)
+module Picshare exposing (main)
 import Browser
 import Html exposing (..)
 import Html.Events exposing (onClick, onInput, onSubmit)
 import Html.Attributes exposing (class, src, placeholder, type_, disabled, value)
--- 1. import Json modules
 import Json.Decode exposing (Decoder, bool, int, list, string, succeed)
 import Json.Decode.Pipeline exposing (hardcoded, required)
+-- 1. add http lib
+import Http
 type alias Id =
   Int
 type alias Photo =
@@ -19,20 +19,14 @@ type alias Photo =
   }
 type alias Model =
   Photo
--- 2. start of photoDecoder function
 photoDecoder : Decoder Photo
 photoDecoder =
--- 3. return type Photo for successfull Json decoding
   succeed Photo
--- 4. you saw that in dogDecoder, in Json string, decode id into Elm int
     |> required "id" int
--- 5. url should be decoded into Elm string
     |> required "url" string
     |> required "caption" string
     |> required "liked" bool
--- 6. this is new because comments is list of strings
     |> required "comments" (list string)
--- 7. newComment is just a helper and it is not stored on server so we do not need it in Json string. Hardcode its value in Photo type to empty Elm string
     |> hardcoded ""
 
 main : Program () Model Msg
@@ -153,4 +147,11 @@ initialModel =
       , liked = False
       , comments = [ "Really nice place!" ]
       , newComment = ""
+    }
+-- 2. fetchFeed constant implementation
+fetchFeed : Cmd Msg
+fetchFeed =
+  Http.get
+    { url = baseUrl ++ "27/kuce.folder/planinarska-kuca-picelj.jpg"
+    , expect = Http.expectJson LoadFeed photoDecoder
     }
