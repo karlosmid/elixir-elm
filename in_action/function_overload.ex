270e52f64bba1bc1784936b1d6703f5bd1fb948e
Karlo Smid <karlo.smid@gmail.com>
2021-02-23 22:02:12 +0100

    gurads in functions.

--- in_action/function_overload.ex
+++ in_action/function_overload.ex
@@ -2,8 +2,11 @@ defmodule Vat do
   @moduledoc """
   Elixir In Action book example for function overloading
   """
-  def price(%{country: :germany, price: price}), do: 1.19 * price
-  def price(%{country: :greece, price: price}), do: 1.24 * price
-  def price(%{country: :croatia, price: price}), do: 1.25 * price
+  def price(%{country: :germany, price: price}), do: calc_total_price(1.19, price)
+  def price(%{country: :greece, price: price}), do: calc_total_price(1.24, price)
+  def price(%{country: :croatia, price: price}), do: calc_total_price(1.25, price)
   def price(unknown), do: {:unknown_country_data, unknown}
+
+  defp calc_total_price(vat, price) when vat > 1.20, do: "Expensive country: " <> "#{vat * price}"
+  defp calc_total_price(vat, price), do: vat * price
 end
